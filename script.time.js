842153862d5b34f4a194a85d85a25d441bed3124
Friedeker <miroslavuhlir@centrum.cz>
2025-01-24 22:14:40 +0100

    Add files via upload

---
+++ script.time.js
@@ -0,0 +1,17 @@
+function updateTime() {
+  const timeElement = document.querySelector('.current-time');
+  if (timeElement) {
+      const now = new Date();
+      const dateString = now.toLocaleDateString('cs-CZ', { 
+          weekday: 'long', // Zobrazí den v týdnu
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+      }); 
+      const timeString = now.toLocaleTimeString('cs-CZ'); // Formát času pro český jazyk
+      timeElement.textContent = `Dnes je ${dateString}, aktuální čas: ${timeString}`;
+  }
+}
+
+setInterval(updateTime, 1000); // Aktualizace každou sekundu
+updateTime(); // První zobrazení času
